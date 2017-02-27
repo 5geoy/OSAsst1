70fffdbf7d5bcfc699c4f13b0c9c59cc6dc84fb5
Lawrence Maceren <lawrencemaceren96@gmail.com>
2017-02-26 21:49:17 -0500

    Fixed errors

--- my_pthread.c
+++ my_pthread.c
@@ -7,11 +7,11 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <signal.h>
-//#include "queue.c"
 #include <ucontext.h>
 #include <string.h>
 #include <stdio.h>
 #include "my_pthread_t.h"
+//#include "queue.c"
 
 // Global variables
 int thread_counter = 1;
@@ -20,6 +20,7 @@ ucontext_t * scheduler_context;
 queue_node * thread_queue_1 = NULL;
 queue_node * thread_queue_2 = NULL;
 queue_node * thread_queue_wait = NULL;
+thread_queue_size_1 = 0;
 int init = 0;
 
 my_pthread_t *current_thread = NULL;
@@ -55,7 +56,7 @@ int my_pthread_create(my_pthread_t * thread, my_pthread_attr_t * attr, void *(*f
 
 	// Set thread id, stack
 	thread->thread_id = thread_counter++;
-	(thread->context)->uc_stack.ss_pp = malloc(100000);
+	(thread->context)->uc_stack.ss_sp = malloc(100000);
 	(thread->context)->uc_stack.ss_size = 100000;
 	(thread->context)->uc_link = NULL;
 
@@ -67,8 +68,6 @@ int my_pthread_create(my_pthread_t * thread, my_pthread_attr_t * attr, void *(*f
 	node->priority = 1;
 	node->thread = thread;
 	thread_queue_1 = enqueue(1,thread,thread_queue_1);
-	
-	}
 
 	return 0;
 }
@@ -85,8 +84,8 @@ void my_pthread_exit(void *value_ptr){
 	queue_node * node1 = peek(thread_queue_1);
 	queue_node * node2 = peek(thread_queue_2);
 	
-	int infirst == 0;
-	int node_id == -1;
+	int infirst = 0;
+	int node_id = -1;
 	
 	// Check each one to remove the thread
 	if (node1 != NULL)
@@ -95,7 +94,7 @@ void my_pthread_exit(void *value_ptr){
 		
 		if (current_thread->thread_id == nodethread->thread_id)
 			{
-				node1 = dequeue(&thread_queue_1));
+				node1 = dequeue(&thread_queue_1);
 				int node_id = (node1->thread)->thread_id;
 				
 				free(node1->thread);
@@ -109,7 +108,7 @@ void my_pthread_exit(void *value_ptr){
 		{
 		my_pthread_t * nodethread = node2->thread;
 		
-		node2 = dequeue(&thread_queue_2));
+		node2 = dequeue(&thread_queue_2);
 		int node_id = (node1->thread)->thread_id;
 		
 		free(node2->thread);
@@ -117,6 +116,7 @@ void my_pthread_exit(void *value_ptr){
 		}
 	
 	// Remove from waiting queue
+	
 	
 	current_thread = NULL;
 	my_pthread_yield();
